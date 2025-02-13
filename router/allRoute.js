05766d48e1a3e0cfda299de67c45d5d8ae13e8c4
lucas user <lucas.momot.rmz@gmail.com>
2025-02-13 15:07:57 +0100

    add gallery

--- router/allRoute.js
+++ router/allRoute.js
@@ -2,7 +2,7 @@ import Route from "./ROute.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "Galerie", "/pages/gallery.html"),
+    new Route("/galerie", "galerie", "/pages/gallery.html"),
 
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
