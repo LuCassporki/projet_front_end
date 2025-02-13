a0f33984ca2aafcf00ad949f9c0f3e42129f6c0c
lucas user <lucas.momot.rmz@gmail.com>
2025-02-13 16:53:23 +0100

    add info connexion

--- router/allRoute.js
+++ router/allRoute.js
@@ -3,6 +3,10 @@ import Route from "./ROute.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "galerie", "/pages/gallery.html"),
+    new Route("/connexion", "connexion", "/pages/auth/connexion.html"),
+    new Route("/s'inscrir", "s'inscrir", "/pages/auth/inscription.html"),
+    new Route("/monCompte", "mon compte", "/pages/auth/monCompte.html"),
+    new Route("/editpassword", "edit password", "/pages/auth/editpassword.html"),
 
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
