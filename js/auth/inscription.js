10408582eab1931f71470fe5ce49b8d013b07202
lucas user <lucas.momot.rmz@gmail.com>
2025-02-14 17:37:39 +0100

    add verif confirm pW2

--- js/auth/inscription.js
+++ js/auth/inscription.js
@@ -2,6 +2,7 @@ const inputNom = document.getElementById("NomInput");
 const inputPrenom = document.getElementById("PrenomInput");
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
+const inputPasswordValid = document.getElementById("ValidatePasswordInput");
 const inputValid = document.getElementById("ValidatePasswordInput");
 const btnValidation = document.getElementById("btn-validation-inscripton");
 
@@ -10,40 +11,28 @@ inputPrenom.addEventListener("keyup", validateForm);
 inputMail.addEventListener("keyup", validateForm);
 inputPassword.addEventListener("keyup", validateForm);
 inputValid.addEventListener("keyup", validateForm);
+// inputPasswordValid.addEventListener("keyup", validateForm);
 
 //Function permettant de valider tout le formulaire
 function validateForm() {
-   const nomok =validateRequired(inputNom);
-   const prenomok =validateRequired(inputPrenom);
-   const mailok =validateMail(inputMail);
+  const nomok = validateRequired(inputNom);
+  const prenomok = validateRequired(inputPrenom);
+  const mailok = validateMail(inputMail);
+  const PWok = validatePW(inputPassword);
+  const PW2ok = validatePW2(inputPassword, inputPasswordValid);
 
-   //permet da cacher le button tant que les champ pas rempli
-    if(nomok && prenomok && mailok){
-        btnValidation.disabled = false;
-    }else{
-        btnValidation.disabled = true;
-    }
+  //permet da cacher le button tant que les champ pas rempli
+  if (nomok && prenomok && mailok && PWok && PW2ok) {
+    btnValidation.disabled = false;
+  } else {
+    btnValidation.disabled = true;
+  }
 }
 
+//Function permettant de valider
 
-//Function permettant de valider un input
-
-
-function validateMail(input){ //Fonction permettant de valider un mail
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const mailUser = input.value;
-    if (mailUser.match(emailRegex)) {
-        input.classList.add("is-valid");
-        input.classList.remove("is-invalid");
-        return true;
-      } else {
-        input.classList.remove("is-valid");
-        input.classList.add("is-invalid");
-        return false;
-      }
-}
-
-function validateRequired(input) { //Fonction permettant de valider un input
+function validateRequired(input) {
+  //Fonction permettant de valider un input
   if (input.value != "") {
     input.classList.add("is-valid");
     input.classList.remove("is-invalid");
@@ -51,6 +40,50 @@ function validateRequired(input) { //Fonction permettant de valider un input
   } else {
     input.classList.remove("is-valid");
     input.classList.add("is-invalid");
+    return false;
+  }
+}
+
+//Fonction permettant de valider un mail
+function validateMail(input) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const mailUser = input.value;
+  if (mailUser.match(emailRegex)) {
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+    return true;
+  } else {
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+    return false;
+  }
+}
+
+//Fonction permettant de valider un pass word
+function validatePW(input) {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+  const PWUser = input.value;
+  if (PWUser.match(passwordRegex)) {
+    input.classList.add("is-valid");
+    input.classList.remove("is-invalid");
+    return true;
+  } else {
+    input.classList.remove("is-valid");
+    input.classList.add("is-invalid");
+    return false;
+  }
+}
+
+// Fonction permettant de valider un pass word
+function validatePW2(inputPassword, inputPasswordValid) {
+  if (inputPasswordValid.value == inputPassword.value) {
+    inputPasswordValid.classList.add("is-valid");
+    inputPasswordValid.classList.remove("is-invalid");
+    return true;
+  } else {
+    inputPasswordValid.classList.remove("is-valid");
+    inputPasswordValid.classList.add("is-invalid");
     return false;
   }
 }
